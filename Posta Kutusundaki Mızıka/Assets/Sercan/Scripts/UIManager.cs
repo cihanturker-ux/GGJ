5e4c51280cb72f61474f731ab21c9f78654f6bc1
Sercan Aydemir <sercan.aydemir001@gmail.com>
2021-01-30 17:15:25 +0300

    ObjectChanger Update

---
+++ Posta Kutusundaki Mızıka/Assets/Sercan/Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Sercan.Scripts
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject interactObjectCanvas;
+
+        public void InteractableObjectCanvas(bool isActive)
+        {
+            interactObjectCanvas.SetActive(isActive);
+        }
+    }
+}
